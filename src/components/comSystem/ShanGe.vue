16cf86521cd1cead7e63cc8d8300004c89d95fae
qiduyu <723179766@qq.com>
2017-09-27 23:48:29 +0800

    栅格原理与权值设计，看父row内容体宽度，浏览器分别率提示不准确

--- src/components/comSystem/ShanGe.vue
+++ src/components/comSystem/ShanGe.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="shan-ge">
     <div class="dj-row">
-      <div class="dj-col-sm-4">1</div>
-      <div class="dj-col-sm-4">2</div>
-      <div class="dj-col-sm-4">3</div>
+      <div class="dj-col-sm-4 dj-col-md-2" style="background: #fff">1</div>
+      <div class="dj-col-sm-4 dj-col-md-8" style="background: pink">2</div>
+      <div class="dj-col-sm-4 dj-col-md-2" style="background: yellow">3</div>
     </div>
   </div>
 </template>
