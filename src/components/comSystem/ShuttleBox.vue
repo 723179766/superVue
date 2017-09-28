e4e3d61034dbba6e3a3cb4032192e74d1b9e7941
qiduyu <723179766@qq.com>
2017-09-29 00:21:43 +0800

    穿梭框样式一期搭建

--- src/components/comSystem/ShuttleBox.vue
+++ src/components/comSystem/ShuttleBox.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="">
-    1232
+    <com-shuttle-box :comProp="comProp"></com-shuttle-box>
   </div>
 </template>
 <script type="text/ecmascript-6">
+  import ComShuttleBox from './components/ComShuttleBox.vue'
   export default {
     created(){
 
     },
+    components:{
+      ComShuttleBox
+    },
     data(){
       return{
-
+        comProp:{
+          "name":1234
+        }
       }
     },
     methods:{
--- src/components/comSystem/components/ComShuttleBox.vue
+++ src/components/comSystem/components/ComShuttleBox.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="">
-
+  <div class="com-shuttle-box">
+    <div class="shuttle-box-left">
+      <p class="shuttle-box-head">待选数据</p>
+      <div class="shuttle-box-body">
+        <label class="data-list" v-bind:class="{'data-list-disabled':item==4}" v-for="item in [1,4,3,4,5,6,1,1,2,1,3,4,1,1,9]">
+          <span class="data-checkbox">
+            <i v-bind:class="{'el-icon-check':item==4}"></i>
+          </span>
+          <span class="data-info">数据1</span>
+        </label>
+      </div>
+    </div>
+    <div class="shuttle-box-middle">
+      <button class="opt-btn"><i class="el-icon-arrow-right"></i></button>
+      <button class="opt-btn"><i class="el-icon-arrow-left"></i></button>
+    </div>
+    <div class="shuttle-box-right">
+      <p class="shuttle-box-head">已选数据</p>
+      <div class="shuttle-box-body">
+        <label class="data-list" v-for="item in [1,2,1]">
+          <span class="data-checkbox"></span>
+          <span class="data-info">数据1</span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -15,9 +38,9 @@
     },
     methods:{
 
+    },
+    props:{
+      comProp: Object
     }
   }
 </script>
-<style lang='scss'>
-
-</style>
