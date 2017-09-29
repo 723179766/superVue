dcf92dbb34f0d48a65b74269af7ffa688de60ffd
qiduyu <723179766@qq.com>
2017-09-30 00:15:18 +0800

    穿梭框基本模板构建，基本逻辑编写

--- src/components/comSystem/ShuttleBox.vue
+++ src/components/comSystem/ShuttleBox.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="">
     <com-shuttle-box :comProp="comProp"></com-shuttle-box>
+    <button class="dj-btn dj-btn-icon-info margin-top-10" @click="getData"><i class="el-icon-star-on icon-left"></i>获取数据</button>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import ComShuttleBox from './components/ComShuttleBox.vue'
   export default {
     created(){
-
+      let vm = this;
+      vm.comProp.leftData = [{id:1,name:'家具'},{id:2,name:'冰雪'},{id:3,name:'火焰'},{id:4,name:'游戏'},{id:5,name:'雪糕'},{id:6,name:'动物'},
+        {id:7,name:'快递'},{id:8,name:'瀑布'},{id:9,name:'军械'},{id:10,name:'电商'},{id:11,name:'七七小鱼'},{id:12,name:'七度鱼'}];
+      vm.comProp.leftData.forEach(function(val,index,arr){
+        vm.$set(val,"selectd",false);
+      });
+      vm.comProp.rightData = [{id:200,name:'张三'},{id:201,name:'理事'}];
+      vm.comProp.rightData.forEach(function(val,index,arr){
+        vm.$set(val,"selectd",false);
+      });
     },
     components:{
       ComShuttleBox
@@ -15,12 +25,17 @@
     data(){
       return{
         comProp:{
-          "name":1234
+          leftTitle: '未添加的联系人',
+          rightTitle: '已存储的联系人',
+          leftData: [],
+          rightData: []
         }
       }
     },
     methods:{
-
+      getData(){
+        alert('你做梦')
+      }
     }
   }
 </script>
--- src/components/comSystem/components/ComShuttleBox.vue
+++ src/components/comSystem/components/ComShuttleBox.vue
@@ -1,43 +1,158 @@
 <template>
   <div class="com-shuttle-box">
-    <div class="shuttle-box-left">
-      <p class="shuttle-box-head">待选数据</p>
+    <div class="shuttle-box">
+      <p class="shuttle-box-head">
+        <span v-if="comProp.leftTitle">{{comProp.leftTitle}}</span>
+        <span v-else>列表1</span>
+      </p>
       <div class="shuttle-box-body">
-        <label class="data-list" v-bind:class="{'data-list-disabled':item==4}" v-for="item in [1,4,3,4,5,6,1,1,2,1,3,4,1,1,9]">
+        <label class="data-list" v-bind:class="{'data-list-disabled':false}" v-for="item in comProp.leftData" @click="leftDataClick(item)">
           <span class="data-checkbox">
-            <i v-bind:class="{'el-icon-check':item==4}"></i>
+            <i v-bind:class="{'el-icon-check':item.selectd==true}"></i>
           </span>
-          <span class="data-info">数据1</span>
+          <span class="data-info">{{item.name}}</span>
         </label>
       </div>
+      <p class="shuttle-box-footer">
+         <span class="data-checkbox" @click="leftAllClick">
+            <i v-bind:class="{'el-icon-check':leftCountMax,'el-icon-minus':leftCountMax=='half'}"></i>
+          </span>
+        <span class="data-info">共{{leftCount}}项</span>
+      </p>
     </div>
+
     <div class="shuttle-box-middle">
-      <button class="opt-btn"><i class="el-icon-arrow-right"></i></button>
-      <button class="opt-btn"><i class="el-icon-arrow-left"></i></button>
+      <button class="opt-btn" :disabled="canMoveRight==false" @click="moveRight"><i class="el-icon-arrow-right"></i></button>
+      <button class="opt-btn" :disabled="canMoveLeft==false" @click="moveLeft"><i class="el-icon-arrow-left"></i></button>
     </div>
-    <div class="shuttle-box-right">
-      <p class="shuttle-box-head">已选数据</p>
+
+    <div class="shuttle-box">
+      <p class="shuttle-box-head">
+        <span v-if="comProp.rightTitle">{{comProp.rightTitle}}</span>
+        <span v-else>列表2</span>
+      </p>
       <div class="shuttle-box-body">
-        <label class="data-list" v-for="item in [1,2,1]">
-          <span class="data-checkbox"></span>
-          <span class="data-info">数据1</span>
+        <label class="data-list" v-bind:class="{'data-list-disabled':false}" v-for="item in comProp.rightData" @click="rightDataClick(item)">
+          <span class="data-checkbox">
+            <i v-bind:class="{'el-icon-check':item.selectd==true}"></i>
+          </span>
+          <span class="data-info">{{item.name}}</span>
         </label>
       </div>
+      <p class="shuttle-box-footer">
+         <span class="data-checkbox" @click="rightAllClick">
+            <i v-bind:class="{'el-icon-check':rightCountMax,'el-icon-minus':rightCountMax=='half'}"></i>
+          </span>
+        <span class="data-info">共{{rightCount}}项</span>
+      </p>
     </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
   export default {
     created(){
-
+      let vm = this;
+      vm.leftCount = vm.comProp.leftData.length;
+      vm.rightCount = vm.comProp.rightData.length;
     },
     data(){
       return{
-
+        leftCount: 0,
+        leftCountMax: false,
+        rightCount: 0,
+        rightCountMax: false,
+        canMoveRight: false,
+        canMoveLeft: false
       }
     },
     methods:{
+      leftDataClick(item){
+        let vm = this,num = 0;
+        item.selectd = !item.selectd;
+        vm.comProp.leftData.forEach(function(val,index,arr){
+          if(val.selectd==true){
+            num++
+          }
+        });
+        if(0<num<vm.leftCount){
+          vm.canMoveRight = true;
+          vm.leftCountMax = 'half';
+        }
+        if(num==0){
+          vm.canMoveRight = false;
+          vm.leftCountMax = false;
+        }
+        if(num==vm.leftCount){
+          vm.canMoveRight = true;
+          vm.leftCountMax = true;
+        }
+      },
+      leftAllClick(){
+        let vm = this;
+        if(vm.leftCountMax==false || vm.leftCountMax=='half'){
+          vm.canMoveRight = true;
+          vm.leftCountMax = true;
+          vm.comProp.leftData.forEach(function(val,index,arr){
+            val.selectd = true;
+          });
+        }else{
+          vm.canMoveRight = false;
+          vm.leftCountMax = false;
+          vm.comProp.leftData.forEach(function(val,index,arr){
+            val.selectd = false;
+          });
+        }
+      },
+      rightDataClick(item){
+        let vm = this,num = 0;
+        item.selectd = !item.selectd;
+        vm.comProp.rightData.forEach(function(val,index,arr){
+          if(val.selectd==true){
+            num++
+          }
+        });
+        if(0<num<vm.rightCount){
+          vm.canMoveLeft = true;
+          vm.rightCountMax = 'half';
+        }
+        if(num==0){
+          vm.canMoveLeft = false;
+          vm.rightCountMax = false;
+        }
+        if(num==vm.rightCount){
+          vm.canMoveLeft = true;
+          vm.rightCountMax = true;
+        }
+      },
+      rightAllClick(){
+        let vm = this;
+        if(vm.rightCountMax==false || vm.rightCountMax=='half'){
+          vm.canMoveLeft = true;
+          vm.rightCountMax = true;
+          vm.comProp.rightData.forEach(function(val,index,arr){
+            val.selectd = true;
+          });
+        }else{
+          vm.canMoveLeft = false;
+          vm.rightCountMax = false;
+          vm.comProp.rightData.forEach(function(val,index,arr){
+            val.selectd = false;
+          });
+        }
+      },
+      moveRight(){
+        let vm = this;
+        console.log('moveRight');
+        console.log(vm.comProp.leftData);
+        vm.comProp.leftData.forEach(function(val,index,arr){
 
+        });
+      },
+      moveLeft(){
+        let vm = this;
+        console.log('moveLeft');
+        console.log(vm.comProp.rightData);
+      }
     },
     props:{
       comProp: Object
