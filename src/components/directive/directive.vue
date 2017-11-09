dd29673e1ceb81ce65bcccf35dfaf4d7b9809ecd
qiduyu <723179766@qq.com>
2017-11-10 00:08:28 +0800

    render-header 终于生效了

--- src/components/directive/directive.vue
+++ src/components/directive/directive.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="">
-    <el-btn :comProp="comProp"></el-btn>
+    <!--<el-btn :comProp="comProp"></el-btn>-->
+    <div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column :render-header="renderHeader" prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column :render-header="renderHeader" prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -14,11 +21,49 @@
     },
     data(){
       return{
-        comProp: 'color-success'
+        comProp: 'color-success',
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
       }
     },
     methods:{
-
+      clickHandler(){
+        alert('终于生效了')
+      },
+      renderHeader: function (h, { column, $index }) {
+        return h('div',[
+          h('span',column.label),
+          h('span', {
+            attrs: {
+              id: 'span'
+            }
+          },'hcy'),
+          h('i',{
+            on: {
+              click: this.clickHandler
+            },
+            style: {
+              color: 'red',
+              fontSize: '14px'
+            }
+          }, '我可以点击？')
+        ])
+      }
     }
   }
 </script>
