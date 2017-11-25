2e4579141ac68526ad337b4610d2b702efde2d6f
qiduyu <723179766@qq.com>
2017-11-26 01:05:55 +0800

    去除首尾空格

--- src/components/newElement/index.vue
+++ src/components/newElement/index.vue
@@ -82,32 +82,14 @@
       <p></p>
       <el-table
         :data="tableData5"
+        :row-class-name="getRowClass"
         style="width: 100%">
-        <el-table-column type="expand" v-if="!noTab">
+        <el-table-column type="expand">
           <template scope="props">
-            <el-form label-position="left" inline class="demo-table-expand">
-              <el-form-item label="商品名称">
-                <span>{{ props.row.name }}</span>
-              </el-form-item>
-              <el-form-item label="所属店铺">
-                <span>{{ props.row.shop }}</span>
-              </el-form-item>
-              <el-form-item label="商品 ID">
-                <span>{{ props.row.id }}</span>
-              </el-form-item>
-              <el-form-item label="店铺 ID">
-                <span>{{ props.row.shopId }}</span>
-              </el-form-item>
-              <el-form-item label="商品分类">
-                <span>{{ props.row.category }}</span>
-              </el-form-item>
-              <el-form-item label="店铺地址">
-                <span>{{ props.row.address }}</span>
-              </el-form-item>
-              <el-form-item label="商品描述">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-            </el-form>
+            <div v-for="item in props.row.brother">
+              <span style="margin-left: 20px">{{item.id}}</span>
+              <span>{{item.name}}</span>
+            </div>
           </template>
         </el-table-column>
         <el-table-column
@@ -117,10 +99,6 @@
         <el-table-column
           label="商品名称"
           prop="name">
-        </el-table-column>
-        <el-table-column
-          label="描述"
-          prop="desc">
         </el-table-column>
       </el-table>
     </div>
@@ -152,7 +130,6 @@
 //      console.log(Boolean(!null))
       var osfipin= '    http://www.cnblogs.com/osfipin/     ';
       console.log(osfipin.replace(/(^\s*)|(\s*$)/g, ""))
-
       var str = '  hcy   '
       console.log(str.replace(/^\s+|\s+$/g,''));
     },
@@ -203,39 +180,41 @@
           }
         ],
         noTab: false,
-        tableData5: [{
-          id: '12987122',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987123',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987125',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987126',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }],
+        tableData5: [
+          {
+            id: '1',
+            name: '黄焖鸡米饭',
+            brother: [
+              {
+                id: '1',
+                name: '特辣黄焖鸡米饭'
+              },
+              {
+                id: '2',
+                name: '微辣黄焖鸡米饭'
+              },
+              {
+                id: '3',
+                name: '超级辣黄焖鸡米饭'
+              }
+            ]
+          },
+          {
+            id: '2',
+            name: '百世可乐',
+            brother: []
+          },
+          {
+            id: '3',
+            name: '油条蛋糕',
+            brother: []
+          },
+          {
+            id: '4',
+            name: '荷兰优质淡奶',
+            brother: []
+          }
+        ],
         dynamicValidateForm: {
           domains: [
             {
@@ -246,6 +225,15 @@
       }
     },
     methods: {
+      getRowClass: function (row, index) {
+        if(row.brother.length>0){
+          console.log(row)
+          console.log(index)
+          return ''
+        }else {
+          return 'hide-expand'
+        }
+      },
       changeCom () {
         if(this.whichShow == 'no'){
           this.whichShow = 'dongTaiCom1'
@@ -332,7 +320,7 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   .head-area{
     box-sizing: border-box;
     height: 80px;
@@ -372,5 +360,10 @@
   }
   .table-cell-hello{
     color: orange;
+  }
+  .hide-expand{
+    .el-table__expand-icon{
+      display: none;
+    }
   }
 </style>
